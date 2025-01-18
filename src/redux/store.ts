9217cdf0c1e2f3047d791a11e57077a9dcf91aad
Sohel Rana <soheltech4@gmail.com>
2025-01-18 21:19:56 +0600

    demo setup

---
+++ src/redux/store.ts
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { baseApi } from './Api/baseApi'
+
+export const store = configureStore({
+    reducer: {
+        // Add the generated reducer as a specific top-level slice
+        [baseApi.reducerPath]: baseApi.reducer,
+      },
+      middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
+})
